bde9b3650a435ce601d0dee4e29271d5ce07efdf
nikita <7jay94@mail.ru>
2024-07-19 10:01:28 +0200

    Initial project spring-jwt

---
+++ src/main/java/t1/springjwtt1/SpringJwtT1Application.java
@@ -0,0 +1,13 @@
+package t1.springjwtt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtT1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtT1Application.class, args);
+    }
+
+}
---
+++ src/test/java/t1/springjwtt1/SpringJwtT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package t1.springjwtt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtT1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
